1ac01c2e3f7134f7caf6a24922903a403f2822f6
MovieaT33 <github.drinking886@passinbox.com>
2025-08-01 12:53:03 +0300

    chore: init

---
+++ profiles/personal/configuration.nix
@@ -0,0 +1,17 @@
+environment.systemPackages = with pkgs; [
+    btop
+    cmatrix
+    eza
+    fastfetch
+    fish
+    git
+    hello
+    kitty
+    libsForQt5.sddm
+    lolcat
+    tldr
+    tmux
+    tree
+    neovim
+    wget
+];
