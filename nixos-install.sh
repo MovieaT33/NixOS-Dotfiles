2f5e6c9a5faa302f247089377937f617ca655ce8
MovieaT33 <github.drinking886@passinbox.com>
2025-08-02 16:35:08 +0300

    feat: add 2 new partitions for NixOS install script

--- nixos-install.sh
+++ nixos-install.sh
@@ -20,14 +20,19 @@ LUKS_PART="${DISK}2"
 # LUKS config
 CRYPT_NAME="cryptroot"
 VG_NAME="vg0"
+
 LV_ROOT="root"
+LV_VAR="var"
+LV_NIXSTORE="nixstore"
 LV_HOME="home"
 LV_TMP="tmp"
-LV_VAR="var"
+LV_VARTMP="vartmp"
 
-SIZE_HOME="5G"
-SIZE_TMP="2G"
 SIZE_VAR="4G"
+SIZE_NIXSTORE="10G"
+SIZE_HOME="5G"
+SIZE_TMP="1G"
+SIZE_VARTMP="1G"
 
 # NixOS config
 NIXOS_CONFIG="/mnt/etc/nixos/configuration.nix"
@@ -57,9 +62,11 @@ vgcreate "$VG_NAME" "/dev/mapper/$CRYPT_NAME"
 # 5. Create logical volumes
 info "05 / 11 | Creating logical volumes..."
 
+lvcreate -L "$SIZE_VAR" "$VG_NAME" -n "$LV_VAR"
+lvcreate -L "$SIZE_NIXSTORE" "$VG_NAME" -n "$LV_NIXSTORE"
 lvcreate -L "$SIZE_HOME" "$VG_NAME" -n "$LV_HOME"
 lvcreate -L "$SIZE_TMP" "$VG_NAME" -n "$LV_TMP"
-lvcreate -L "$SIZE_VAR" "$VG_NAME" -n "$LV_VAR"
+lvcreate -L "$SIZE_VARTMP" "$VG_NAME" -n "$LV_VARTMP"
 
 # Use remaining space for root
 lvcreate -l 100%FREE "$VG_NAME" -n "$LV_ROOT"
@@ -68,9 +75,11 @@ lvcreate -l 100%FREE "$VG_NAME" -n "$LV_ROOT"
 info "06 / 11 | Formatting logical volumes..."
 
 mkfs.ext4 "/dev/$VG_NAME/$LV_ROOT"
+mkfs.ext4 "/dev/$VG_NAME/$LV_VAR"
+mkfs.ext4 "/dev/$VG_NAME/$LV_NIXSTORE"
 mkfs.ext4 "/dev/$VG_NAME/$LV_HOME"
 mkfs.ext4 "/dev/$VG_NAME/$LV_TMP"
-mkfs.ext4 "/dev/$VG_NAME/$LV_VAR"
+mkfs.ext4 "/dev/$VG_NAME/$LV_VARTMP"
 
 # 7. Mount partitions
 info "07 / 11 | Mounting partitions..."
@@ -79,10 +88,12 @@ mount "/dev/$VG_NAME/$LV_ROOT" /mnt
 mkdir -p /mnt/boot
 mount "$EFI_PART" /mnt/boot
 
-mkdir -p /mnt/{home,tmp,var}
+mkdir -p /mnt/{var,nix/store,home,tmp,var/tmp}
+mount "/dev/$VG_NAME/$LV_VAR" /mnt/var
+mount "/dev/$VG_NAME/$LV_NIXSTORE" /mnt/nix/store
 mount "/dev/$VG_NAME/$LV_HOME" /mnt/home
 mount "/dev/$VG_NAME/$LV_TMP" /mnt/tmp
-mount "/dev/$VG_NAME/$LV_VAR" /mnt/var
+mount "/dev/$VG_NAME/$LV_VARTMP" /mnt/var/tmp
 
 # 8. Generate NixOS hardware configuration
 info "08 / 11 | Generating NixOS hardware configuration..."
