bfbef22dd00b28fc8e5d5a818a1ddd9f7764d11f
MovieaT33 <github.drinking886@passinbox.com>
2025-08-02 15:55:29 +0300

    fix: firewall temporary testing

--- system/security/firewall.nix
+++ system/security/firewall.nix
@@ -1,45 +1,8 @@
 { config, pkgs, ... }:
 
-let
-  torTransPort = 9040;
-  torDnsPort = 5353;
-in
 {
-  services.tor = {
-    enable = true;
-    settings = {
-      VirtualAddrNetworkIPv4 = "10.192.0.0/10";
-      AutomapHostsOnResolve = true;
-      TransPort = torTransPort;
-      DNSPort = torDnsPort;
-      SocksPort = "9050";
-    };
-  };
-
   networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [];
-
   networking.firewall.extraCommands = ''
-    iptables -F
-    iptables -t nat -F
-
-    iptables -P INPUT DROP
-    iptables -P FORWARD DROP
-    iptables -P OUTPUT DROP
-
-    iptables -A INPUT -i lo -j ACCEPT
-    iptables -A OUTPUT -o lo -j ACCEPT
-
-    iptables -A OUTPUT -m owner --uid-owner tor -j ACCEPT
-
-    iptables -A INPUT -p tcp --dport 22 -j DROP
-
-    iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports ${toString torDnsPort}
-    iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports ${toString torTransPort}
-
-    iptables -t nat -A OUTPUT -p tcp --syn ! -o lo -m owner ! --uid-owner tor -j REDIRECT --to-ports ${toString torTransPort}
-
-    iptables -A OUTPUT ! -o lo ! -m owner --uid-owner tor -j DROP
   '';
 
   boot.kernelParams = [ "ipv6.disable=1" ];
