221e9b02b8d844abe60c8fc6d34182fda2940ceb
MovieaT33 <github.drinking886@passinbox.com>
2025-08-03 18:18:49 +0300

    refactor: temporary disable firewall

--- system/security/firewall.nix
+++ system/security/firewall.nix
@@ -1,29 +1,27 @@
 { config, ... }:
 
 {
-  networking.firewall.enable = true;
+  # networking.firewall.enable = true;
 
-  networking.firewall.allowedTCPPorts = [ 9050 ];
+  # networking.firewall.allowedTCPPorts = [ 9050 ];
 
   # TODO: use builtins.readFile
-  networking.firewall.extraCommands = ''
-    iptables -P INPUT DROP
-    iptables -P OUTPUT DROP
-    iptables -P FORWARD DROP
+  # networking.firewall.extraCommands = ''
+  #   iptables -P INPUT DROP
+  #   iptables -P OUTPUT DROP
+  #   iptables -P FORWARD DROP
 
-    iptables -A INPUT -i lo -j ACCEPT
-    iptables -A OUTPUT -o lo -j ACCEPT
+  #   iptables -A INPUT -i lo -j ACCEPT
+  #   iptables -A OUTPUT -o lo -j ACCEPT
 
-    iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+  #   iptables -A OUTPUT -m owner --uid-owner tor -j ACCEPT
+  # '';
 
-    iptables -A OUTPUT -m owner --uid-owner tor -j ACCEPT
-  '';
-
-  networking.firewall.extraStopCommands = ''
-    iptables -P INPUT ACCEPT
-    iptables -P OUTPUT ACCEPT
-    iptables -P FORWARD ACCEPT
-  '';
+  # networking.firewall.extraStopCommands = ''
+  #   iptables -P INPUT ACCEPT
+  #   iptables -P OUTPUT ACCEPT
+  #   iptables -P FORWARD ACCEPT
+  # '';
 
   services.tor = {
     enable = true;
