ae153a193f2a98ae1bd2cbbf518aa927189b8290
MovieaT33 <github.drinking886@passinbox.com>
2025-08-03 18:14:12 +0300

    fix: sudo power

--- system/security/sudo.nix
+++ system/security/sudo.nix
@@ -9,6 +9,10 @@
           command = "/run/current-system/sw/bin/reboot";
           options = [ "NOPASSWD" ];
         }
+        {
+          command = "/run/current-system/sw/bin/shutdown";
+          options = [ "NOPASSWD" ];
+        }
       ];
     }
   ];
