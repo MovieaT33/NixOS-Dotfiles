7d4cdcd24c2e5e9ee50924bb2d81f723fc83b195
MovieaT33 <github.drinking886@passinbox.com>
2025-08-03 18:31:22 +0300

    feat: add DNS Tor service

--- system/security/tor.nix
+++ system/security/tor.nix
@@ -1,11 +1,32 @@
 { config, ... }:
 
 {
-  services.tor = {
-    enable = true;
-    settings = {
-      SOCKSPort = "9050";
-      SocksPolicy = [ "accept 127.0.0.1" ];
+  services = {
+    tor = {
+      enable = true;
+      client.dns.enable = true;
+      settings.DNSPort = [ {
+        addr = "127.0.0.1";
+        port = 53;
+      } ];
+    };
+    resolved = {
+      enable = true;
+      dns = [ "127.0.0.1" ];
+      fallbackDns = [ ];
+      extraConfig = ''
+        DNSStubListener=no
+      '';
     };
   };
+  
+  networking.nameservers = [ "127.0.0.1" ];
+
+  # services.tor = {
+  #   enable = true;
+  #   settings = {
+  #     SOCKSPort = "9050";
+  #     SocksPolicy = [ "accept 127.0.0.1" ];
+  #   };
+  # };
 }
