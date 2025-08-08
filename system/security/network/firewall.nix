2f71459a605bc8e38f47bbb8fe354957984ebf33
MovieaT33 <github.drinking886@passinbox.com>
2025-08-08 15:18:54 +0300

    0.8.9 3

--- system/security/network/firewall.nix
+++ system/security/network/firewall.nix
@@ -3,15 +3,16 @@
 {
   networking.firewall.enable = true;
 
+  networking.firewall.allowPing = false;
+  networking.firewall.checkReversePath = true;
+
   networking.firewall.allowedTCPPorts = [ 443 ];
   networking.firewall.allowedUDPPorts = [ 53 ];
-
-  # networking.firewall.interfaces.lo.allowedTCPPorts = [ ];
-  # networking.firewall.interfaces.lo.allowedUDPPorts = [ ];
 
   # networking.firewall.interfaces.enp1s0.allowedTCPPorts = [ ];
   # networking.firewall.interfaces.enp1s0.allowedUDPPorts = [ ];
 
-  networking.firewall.allowPing = false;
-  networking.firewall.checkReversePath = true;
+  networking.firewall.extraCommands = ''
+    iptables -A OUTPUT -p tcp --dport 80 -j REJECT
+  '';
 }
