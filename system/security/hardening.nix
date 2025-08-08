a1d25837bec74dc13878846bf2e5f60c42ddad76
MovieaT33 <github.drinking886@passinbox.com>
2025-08-08 15:53:14 +0300

    0.8.12 2

--- system/security/hardening.nix
+++ system/security/hardening.nix
@@ -2,6 +2,7 @@
 
 {
   systemd.tmpfiles.rules = [
-    "d /etc/nixos 0750 root wheel -"
+    "d /etc/nixos 0750 root wheel -",
+    # "d /etc/nixos 0700 root root -"
   ];
 }
--- system/security/root.nix
+++ system/security/root.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [ shadow ];
 
-  users.users.root.password = "*";
+  users.users.root.password = "!";
 
   users.users.root = {
     shell = "${pkgs.shadow}/bin/nologin";
