459fd06ca8ca7f3617640c410861387370d5d36d
MovieaT33 <github.drinking886@passinbox.com>
2025-08-13 14:01:49 +0300

    0.9.5.11

--- system/default.nix
+++ system/default.nix
@@ -1,24 +1,25 @@
-{ lib, config, ... }:
-
-with lib; with builtins;
-
-let
-  getDir = dir:
-    mapAttrs
-      (file: type: if type == "directory"
-                   then getDir (dir + "/" + file)
-                   else type)
-      (readDir dir);
-
-  files = dir:
-    let
-      collectFiles = mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir);
-    in
-      builtins.attrValues collectFiles;
-
-  nixFiles = dir:
-    map (file: dir + "/" + file)
-      (filter (file: hasSuffix ".nix" file) (files dir));
-
-in
-{ imports = nixFiles ./.; }
+{}
+# { lib, config, ... }:
+# 
+# with lib; with builtins;
+# 
+# let
+#   getDir = dir:
+#     mapAttrs
+#       (file: type: if type == "directory"
+#                    then getDir (dir + "/" + file)
+#                    else type)
+#       (readDir dir);
+# 
+#   files = dir:
+#     let
+#       collectFiles = mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir);
+#     in
+#       builtins.attrValues collectFiles;
+# 
+#   nixFiles = dir:
+#     map (file: dir + "/" + file)
+#       (filter (file: hasSuffix ".nix" file) (files dir));
+# 
+# in
+# { imports = nixFiles ./.; }
