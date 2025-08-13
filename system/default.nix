e6c8e8d7b6e2b8c5ebea78f332834e4e72ac3f60
MovieaT33 <github.drinking886@passinbox.com>
2025-08-13 14:13:43 +0300

    0.9.5.15

--- system/default.nix
+++ system/default.nix
@@ -1,7 +1,22 @@
-{ lib, ... }:
-
-{
-  imports = lib.fileset.toList (
-    lib.fileset.fileFilter (file: file.name == "default.nix") ./.
-  );
-}
+let
+  importDir = dir:
+    let
+      entries = builtins.readDir dir;
+    in
+      builtins.listToAttrs (
+        [ for name in builtins.attrNames entries
+          : { name = builtins.replaceStrings [".nix"] [""] name;
+              value = import (dir + "/${name}");
+            }
+          | entries.${name} == "regular"
+          && name != "default.nix"
+          && builtins.match ".*\\.nix$" name != null
+        ]
+        ++
+        [ for name in builtins.attrNames entries
+          : { name = name; value = importDir (dir + "/${name}"); }
+          | entries.${name} == "directory"
+        ]
+      );
+in
+  importDir ./. 
