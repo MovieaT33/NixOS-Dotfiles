4673af04f6dbd371d2389ecd68f1a141739b8bb8
MovieaT33 <github.drinking886@passinbox.com>
2025-08-13 14:10:39 +0300

    0.9.5.14

--- system/default.nix
+++ system/default.nix
@@ -1,25 +1,7 @@
-let
-  dropNix = name:
-    let m = builtins.match "^(.*)\\.nix$" name;
-    in if m == null then name else builtins.elemAt m 0;
+{ lib, ... }:
 
-  importDir = dir:
-    let
-      listing = builtins.readDir dir;
-      names   = builtins.attrNames listing;
-
-      nixFiles = builtins.filter (n:
-        listing.${n} == "regular"
-        && builtins.match ".*\\.nix$" n != null
-        && n != "default.nix"
-      ) names;
-
-      subdirs = builtins.filter (n: listing.${n} == "directory") names;
-    in
-      builtins.listToAttrs (
-        (map (n: { name = dropNix n; value = import (dir + "/${n}"); }) nixFiles)
-        ++
-        (map (d: { name = d; value = importDir (dir + "/${d}"); }) subdirs)
-      );
-in
-  importDir ./. 
+{
+  imports = lib.fileset.toList (
+    lib.fileset.fileFilter (file: file.name == "default.nix") ./.
+  );
+}
