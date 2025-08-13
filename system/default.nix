e7fc7b654b9eab6bcdefcda5b716c298c5385bea
MovieaT33 <github.drinking886@passinbox.com>
2025-08-13 13:48:27 +0300

    0.9.5.6

--- system/default.nix
+++ system/default.nix
@@ -1,3 +1,17 @@
 { config, ... }:
 
+let
+  importAll = dir:
+    let
+      entries = builtins.attrNames (builtins.readDir dir);
+    in
+      builtins.concatLists (map (name:
+        let path = dir + "/" + name;
+        in if builtins.isDirectory path
+           then importAll path
+           else if lib.strings.hasSuffix ".nix" name
+                then [ (import path) ]
+                else []
+      ) entries);
+in
 { imports = importAll ./.; }
