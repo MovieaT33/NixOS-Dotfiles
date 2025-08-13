13245803992bc404d8846db9a884ad8a8e9448e3
MovieaT33 <github.drinking886@passinbox.com>
2025-08-13 13:29:54 +0300

    0.9.5 2

--- app/config/neovim.nix
+++ app/config/neovim.nix
@@ -5,7 +5,10 @@
     enable = true;
     configure = {
       customRC = ''
+        nnoremap <Tab> <Esc>
+        vnoremap <Tab> <Esc>
         inoremap <Tab> <Esc>
+
         set number
         syntax on
       '';
