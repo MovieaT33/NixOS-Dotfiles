dde90b1d80275f33a65971a8d109da2678c20436
MovieaT33 <github.drinking886@passinbox.com>
2025-08-13 15:32:42 +0300

    0.9.8.3

--- app/config/hyprland.nix
+++ app/config/hyprland.nix
@@ -3,11 +3,6 @@
 {
   services.xserver.enable = true;
 
-  services.xserver.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-  
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
--- app/config/sddm.nix
+++ app/config/sddm.nix
@@ -2,6 +2,11 @@
 
 {
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.defaultSession = "hyprland";
+  services.displayManager = {
+    defaultSession = "hyprland";
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+  };
 }
