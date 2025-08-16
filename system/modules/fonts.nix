9b22899ed6d2c7aa17872646d96d92a5c9498803
MovieaT33 <github.drinking886@passinbox.com>
2025-08-16 13:30:47 +0300

    0.9.10.11

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  fonts.packages = [
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 }
