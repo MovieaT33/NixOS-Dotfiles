c32408a7cba9311cd585aee9b909b8bbfe9f037e
MovieaT33 <github.drinking886@passinbox.com>
2025-08-16 13:54:34 +0300

    0.9.10.18

--- app/config/home/kitty.nix
+++ app/config/home/kitty.nix
@@ -6,8 +6,4 @@
   ];
 
   home.file.".config/kitty/kitty.conf".source = ../dotfiles/kitty.conf;
-
-  programs.zsh.ohMyZsh.enable = true;
-  programs.zsh.ohMyZsh.theme = "afowler";
-  programs.zsh.ohMyZsh.plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
 }
--- app/config/zsh.nix
+++ app/config/zsh.nix
@@ -1,10 +1,20 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+  environment.systemPackages = [ eza ];
+
   programs.zsh = {
     enable = true;
     shellAliases = {
+      l = "exa -l --icons";
+      ls = "exa -la --icons";
       j = "just";
+    };
+
+    ohMyZsh = {
+      enable = true;
+      theme = "afowler";
+      plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
     };
   };
 }
