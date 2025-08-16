9cfe6e583bc58739f4648e24f3ebcdb83a51d725
MovieaT33 <github.drinking886@passinbox.com>
2025-08-16 13:11:22 +0300

    0.9.10.5

--- app/config/home/kitty.nix
+++ app/config/home/kitty.nix
@@ -1,9 +1,22 @@
 { config, pkgs, ... }:
 
+let
+  catppuccinKitty = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/kitty/main/themes/mocha.conf";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+in
 {
   home.packages = with pkgs; [
     kitty
   ];
 
   home.file.".config/kitty/kitty.conf".source = ../dotfiles/kitty.conf;
+
+  programs.kitty = {
+    enable = true;
+    extraConfig = ''
+      include ${catppuccinKitty}
+    '';
+  };
 }
