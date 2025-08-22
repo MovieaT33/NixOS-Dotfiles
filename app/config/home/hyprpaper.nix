6ad13e3103226d993aea1eb8b3aec1346edde662
MovieaT33 <github.drinking886@passinbox.com>
2025-08-22 21:14:28 +0300

    0.9.12.22

--- app/config/home/hyprpaper.nix
+++ app/config/home/hyprpaper.nix
@@ -1,13 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  services.hyprpaper = {
-    enable = true;
-    package = pkgs.hyprpaper;
-    settings = {
-      wallpaper = [
-        "Virtual-1, /etc/nixos/img/Alps - 1920x1200.jpg"
-      ];
-    };
-  };
+  home.packages = with pkgs; [ hyprpaper ];
 }
--- profiles/personal/config.nix
+++ profiles/personal/config.nix
@@ -7,5 +7,6 @@
     ../../app/config/hyprland.nix
     ../../app/config/sddm.nix
   ];
+
   home-manager.users.mvt33 = import ./home.nix;
 }
