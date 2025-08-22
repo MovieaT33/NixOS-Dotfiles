093c612ca06155e8c1f4d659d484367f07ce6567
MovieaT33 <github.drinking886@passinbox.com>
2025-08-22 14:37:51 +0300

    0.9.11.8

--- system/security/network/dns.nix
+++ system/security/network/dns.nix
@@ -3,7 +3,5 @@
 {
   services.resolved.enable = true;
   networking.nameservers = [ "9.9.9.11" ];
-  networking.resolved.extraConfig = ''
-    FallbackDNS=
-  '';
+  services.resolved.fallbackDns = [];
 }
