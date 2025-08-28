d4a72188363a33e9a91ce156623030fe2320d0f8
MovieaT33 <github.drinking886@passinbox.com>
2025-08-28 14:32:26 +0300

    0.9.19.11.3-rc2

--- modules/system/security/journald.nix
+++ modules/system/security/journald.nix
@@ -2,19 +2,18 @@
 
 {
   services.journald = {
-    storage = "volatile";   # store logs in memory
-    compress = true;
-    forwardToSysLog = true;
-
-    systemMaxUse = 512 * 1024 * 1024;     # 512 MB total
-    systemMaxFileSize = 64 * 1024 * 1024; # 64 MB per file
-    runtimeMaxUse = 256 * 1024 * 1024;    # 256 MB in RAM for volatile logs
-
+    storage = "volatile";     # store logs in memory
     audit = true;
-
-    upload.enable = false;                # disable remote log upload
+    upload.enable = false;    # disable remote log upload
 
     extraConfig = ''
+      Compress=yes
+      ForwardToSyslog=yes
+
+      SystemMaxUse=512M
+      SystemMaxFileSize=64M
+      RuntimeMaxUse=256M
+
       MaxRetentionSec=1month
       MaxFileSec=1week
     '';
