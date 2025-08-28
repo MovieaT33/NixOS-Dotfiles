10459a7527aea12802efc34774169b4e19f4d0b4
MovieaT33 <github.drinking886@passinbox.com>
2025-08-28 14:00:57 +0300

    0.9.19.10-rc1

--- modules/app/config/zsh.nix
+++ modules/app/config/zsh.nix
@@ -1,46 +1,45 @@
 { ... }:
 
 {
-  imports = [ ./eza.nix ]; 
+  imports = [ ./atuin.nix ./eza.nix ]; 
 
   programs.zsh = {
     enable = true;
     shellAliases = {
-      sudo = "doas";
+      # Utilities
       l  = "exa -l --icons --git-ignore";
-      ls = "exa -lG --icons -a --group-directories-first -ghHiMOS --total-size -o --git";
-
+      ll = "exa -lG --icons -a --group-directories-first -ghHiMOS --total-size -o --git";
       v  = "nvim";
 
       # Cryptsetup
-      luks-add =    "sudo cryptsetup luksAddKey /dev/vda2";
-      luks-remove = "sudo cryptsetup luksRemoveKey /dev/vda2";
+      luks-add    = "sudo cryptsetup luksAddKey /dev/vda2";     # add a new key  (rotate periodically)
+      luks-remove = "sudo cryptsetup luksRemoveKey /dev/vda2";  # remove old key (rotate periodically)
 
       # Security
-      systemd-security = "systemd-analyze security";
+      sudo             = "doas";
       lynis-security   = "sudo lynis audit system";
-
-      # Nix
-      info = "nix-info -m";
+      systemd-security = "systemd-analyze security";
 
       # Dotfiles
       d  =  "cd /etc/nixos";
       j  =  "just";
+      D  = "cd /etc/nixos; just";
       dj = "cd /etc/nixos; just";
 
-      ddd = "cd /etc/nixos; just";
-      D   = "cd /etc/nixos; just";
-
       # System
-      r = "sudo reboot now";
-      s = "sudo shutdown now";
+      system-info = "nix-info -m";
+      r           = "sudo reboot now";
+      s           = "sudo shutdown now";
     };
 
     ohMyZsh = {
       enable = true;
-
       theme = "afowler";
       plugins = [ "git" ];
     };
+
+    shellInit = ''
+      eval "$(atuin init zsh)"
+    '';
   };
 }
