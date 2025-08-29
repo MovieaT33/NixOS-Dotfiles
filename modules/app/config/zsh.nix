a410ead9de3fbaa66dcdb3f0528f5294d6103d63
MovieaT33 <github.drinking886@passinbox.com>
2025-08-29 20:51:50 +0300

    0.9.22.6

--- modules/app/config/zsh.nix
+++ modules/app/config/zsh.nix
@@ -4,6 +4,6 @@
   imports = [ ./eza.nix ];
 
   # Home-manager
-  home-manager.users.mvt33 = import ./home-manager/zsh.nix;
-  home-manager.users.mvt33 = import ./home-manager/command-not-found.nix;
+  home-manager.users.mvt33 = 
+    (import ./home-manager/zsh.nix) // (./home-manager/command-not-found.nix;)
 }
