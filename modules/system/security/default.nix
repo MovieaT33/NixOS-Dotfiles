0e9726822582cf6942ee6ec8c206b817f2a63239
MovieaT33 <github.drinking886@passinbox.com>
2025-08-30 14:09:53 +0300

    0.9.23.14

--- modules/system/security/default.nix
+++ modules/system/security/default.nix
@@ -1,32 +1,32 @@
 { ... }:
 
 let
-  authDir = "./auth";
-  networkDir = "./network";
-  servicesDir = "./services";
+  authDir = "auth";
+  networkDir = "network";
+  servicesDir = "services";
 in
 {
   imports = [
     # Authentication
-    ${authDir}/doas.nix
-    ${authDir}/pam.nix
-    ${authDir}/root.nix
+    "${authDir}/doas.nix"
+    "${authDir}/pam.nix"
+    "${authDir}/root.nix"
 
     # Network
-    ${networkDir}/dns.nix
-    ${networkDir}/firewall.nix
-    ${networkDir}/ipv6.nix
+    "${networkDir}/dns.nix"
+    "${networkDir}/firewall.nix"
+    "${networkDir}/ipv6.nix"
 
     # Services
-    ${servicesDir}/antivirus.nix
-    ${servicesDir}/apparmor.nix
-    ${servicesDir}/audit.nix
-    ${servicesDir}/dbus.nix
-    ${servicesDir}/journald.nix
-    ${servicesDir}/logrotate.nix
-    ${servicesDir}/rtkit.nix
-    ${servicesDir}/sysstat.nix
-    # ${servicesDir}/usbguard.nix
+    "${servicesDir}/antivirus.nix"
+    "${servicesDir}/apparmor.nix"
+    "${servicesDir}/audit.nix"
+    "${servicesDir}/dbus.nix"
+    "${servicesDir}/journald.nix"
+    "${servicesDir}/logrotate.nix"
+    "${servicesDir}/rtkit.nix"
+    "${servicesDir}/sysstat.nix"
+    # "${servicesDir}/usbguard.nix"
 
     # Main
     ./boot.nix
