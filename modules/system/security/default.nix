bf8e8d80542d170cdd34120f34ca97e63d3d9254
MovieaT33 <github.drinking886@passinbox.com>
2025-08-30 14:17:33 +0300

    0.9.23.20

--- modules/system/security/default.nix
+++ modules/system/security/default.nix
@@ -8,25 +8,25 @@ in
 {
   imports = [
     # Authentication
-    authDir / "doas.nix"
-    authDir / "pam.nix"
-    authDir / "root.nix"
+    "${authDir}/doas.nix"
+    "${authDir}/pam.nix"
+    "${authDir}/root.nix"
 
     # Network
-    networkDir / "dns.nix"
-    networkDir / "firewall.nix"
-    networkDir / "ipv6.nix"
+    "${networkDir}/dns.nix"
+    "${networkDir}/firewall.nix"
+    "${networkDir}/ipv6.nix"
 
     # Services
-    servicesDir / "antivirus.nix"
-    servicesDir / "apparmor.nix"
-    servicesDir / "audit.nix"
-    servicesDir / "dbus.nix"
-    servicesDir / "journald.nix"
-    servicesDir / "logrotate.nix"
-    servicesDir / "rtkit.nix"
-    servicesDir / "sysstat.nix"
-    # servicesDir / "usbguard.nix"
+    "${servicesDir}/antivirus.nix"
+    "${servicesDir}/apparmor.nix"
+    "${servicesDir}/audit.nix"
+    "${servicesDir}/dbus.nix"
+    "${servicesDir}/journald.nix"
+    "${servicesDir}/logrotate.nix"
+    "${servicesDir}/rtkit.nix"
+    "${servicesDir}/sysstat.nix"
+    # "${servicesDir}/usbguard.nix"
 
     # Main
     ./boot.nix
