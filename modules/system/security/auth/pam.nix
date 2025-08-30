cefbcefd4ae5e509972c2f37be8771be1757e69c
MovieaT33 <github.drinking886@passinbox.com>
2025-08-30 15:45:37 +0300

    0.9.24.3

--- modules/system/security/auth/pam.nix
+++ modules/system/security/auth/pam.nix
@@ -1,8 +1,8 @@
 { ... }:
 
 {
-  # TODO: PAM module
-  security.pam.services.login.text = ''
+  # FIXME: PAM module
+  security.pam.services.su.text = ''
     auth       required      pam_faillock.so preauth silent deny=5 unlock_time=900
     auth       [default=die] pam_faillock.so authfail deny=5 unlock_time=900
     account    required      pam_faillock.so
