0dc9b61e10bb18c7fb0a19f6e33b463508dd5f26
MovieaT33 <github.drinking886@passinbox.com>
2025-08-30 15:57:46 +0300

    0.9.24.6

--- modules/system/security/auth/pam.nix
+++ modules/system/security/auth/pam.nix
@@ -1,12 +1,13 @@
 { ... }:
 
 {
+  # TODO: Add `login`, `passwd`, ... modules
   # FIXME: PAM module
   security.pam.services.su.text = ''
     auth       required      pam_faillock.so preauth silent deny=5 unlock_time=900
-    auth       [default=die] pam_faillock.so authfail deny=5 unlock_time=900
-    account    required      pam_faillock.so
     @include common-auth
+    auth       [default=bad] pam_faillock.so authfail deny=5 unlock_time=900
+    account    required      pam_faillock.so
     @include common-account
     @include common-session
     @include common-password
