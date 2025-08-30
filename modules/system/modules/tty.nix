5039e401484f85c2c8fa5466df4f6a7fcdaf6e27
MovieaT33 <github.drinking886@passinbox.com>
2025-08-30 14:41:06 +0300

    0.9.23.5

--- modules/system/modules/tty.nix
+++ modules/system/modules/tty.nix
@@ -2,8 +2,9 @@
 
 {
   boot.kernelParams = [
-    # "console=tty0"    # console output
-    "video=1920x1200,921600"   # set resolution
-    # "quiet"           # reduce messages
+    # "console=tty0"        # console output
+    "console=ttyS0,115200"  # console output
+    "video=1920x1200@60"    # set resolution
+    # "quiet"               # reduce messages
   ];
 }
