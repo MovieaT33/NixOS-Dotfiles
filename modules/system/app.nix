734ef3c8b17bc093bfee65a87c2e854fd6da83e9
Movie <github.drinking886@passinbox.com>
2025-08-31 14:26:34 +0300

    Update app.nix

--- modules/system/app.nix
+++ modules/system/app.nix
@@ -4,5 +4,6 @@
   environment.systemPackages = with pkgs; [
     git just                                # repository and system
     lynis kernel-hardening-checker          # security
+    # TODO: Seperate `lynis` and `kernel-hardening-checker` into configurations
   ];
 }
