3b1e0bb3b43df8a956fb0e10145d253f47eec858
MovieaT33 <github.drinking886@passinbox.com>
2025-09-20 16:33:59 +0300

    0.10.2.17: fix initrd.nix

--- modules/system/boot/boot/initrd.nix
+++ modules/system/boot/boot/initrd.nix
@@ -7,8 +7,27 @@
       # "ahci"                          # SATA drives
       # "xhci_pci"                      # USB 3.0 / 3.1
       # "sr_mod"                        # SCSI CD / DVD
+
+      # region [ Virtio ]
+      "virtio_blk"      # block device (disk)
+      "virtio_pci"      # peripheral component interconnect
+      # "virtio_mmio"   # memory mapped i/o
+      # "virtio_scsi"   # small computer system interface
+      "virtio_net"      # virtio network interface
+      # "9p"            # 9p filesystem
+      # "9pnet_virtio"  # network transport for 9p
+      # endregion
     ];
-    kernelModules = [ "dm-snapshot" ];  # Device-mapper snapshot
+    kernelModules = [
+      "dm-snapshot"   # Device-mapper snapshot
+
+      # region [ Virtio ]
+      "virtio_balloon"  # dynamic RAM management for VM
+      "virtio_console"  # virtual console for communication with the hypervisor
+      "virtio_gpu"      # virtual GPU
+      "virtio_rng"      # virtual random number generator
+      # endregion
+    ];
   };
   # endregion
 
@@ -21,24 +40,7 @@
   };
   # endregion
 
-  # region [ QEMU profile ]
-  boot.initrd = {
-    availableKernelModules = [
-      "virtio_blk"      # block device (disk)
-      "virtio_pci"      # peripheral component interconnect
-      # "virtio_mmio"   # memory mapped i/o
-      # "virtio_scsi"   # small computer system interface
-      "virtio_net"      # virtio network interface
-      # "9p"            # 9p filesystem
-      # "9pnet_virtio"  # network transport for 9p
-    ];
-    kernelModules = [
-      "virtio_balloon"  # dynamic RAM management for VM
-      "virtio_console"  # virtual console for communication with the hypervisor
-      "virtio_gpu"      # virtual GPU
-      "virtio_rng"      # virtual random number generator
-    ];
-  };
+  # region [ Virtio ]
   boot.kernelModules = [ "kvm-intel" ];
   # endregion
 
