6b3721aa393ccb9c5db9492092e806e52221b85c
MovieaT33 <github.drinking886@passinbox.com>
2025-09-28 14:00:24 +0300

    0.10.9.9: fix

--- modules/app/config/sddm.nix
+++ modules/app/config/sddm.nix
@@ -6,6 +6,18 @@
     autoLogin.user = "mvt33";
   };
 
+  # TODO: Remove this once
+  services.xserver = {
+    enable = false;
+    monitorSection = ''
+      Section "Monitor"
+        Identifier "HDMI-1"
+        Option "PreferredMode" "1920x1200"
+      EndSection
+    '';
+    xkb.layout = "us";   # TODO: Check is work correctly
+  };
+
   environment.systemPackages = with pkgs; [
     sddm-astronaut
   ];
