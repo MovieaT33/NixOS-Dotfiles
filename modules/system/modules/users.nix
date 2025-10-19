f06dbf5d19b9ac07f62170407f3a52da215c2aad
MovieaT33 <github.drinking886@passinbox.com>
2025-10-19 16:08:39 +0300

    1.1.2: fix

--- modules/system/modules/users.nix
+++ modules/system/modules/users.nix
@@ -2,16 +2,19 @@
 
 {
   home-manager.users."mvt33" = import ../../app/config/home-modules/shells/zsh.nix;
-  home-manager.users."root" = import ../../app/config/home-modules/shells/zsh.nix;
+  home-manager.users."mvt33" = import ../../app/config/home-modules/shells/utilities.nix;
+
+  # home-manager.users."root" = import ../../app/config/home-modules/shells/zsh.nix;
+  # home-manager.users."root" = import ../../app/config/home-modules/shells/utilities.nix;
 
   users.users = {
     "mvt33" = {
       shell = "${pkgs.zsh}/bin/zsh";
       ignoreShellProgramCheck = true;
     };
-    "root" = {
-      shell = "${pkgs.zsh}/bin/zsh";
-      ignoreShellProgramCheck = true;
-    };
+    # "root" = {
+    #   shell = "${pkgs.zsh}/bin/zsh";
+    #   ignoreShellProgramCheck = true;
+    # };
   };
 }
